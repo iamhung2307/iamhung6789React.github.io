173952483bc95be1ee2f23c4098b4abe30cbb716
Nguyễn Vĩnh Hưng <113844426+iamhung2307@users.noreply.github.com>
2022-10-01 21:20:27 +0700

    f

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -5,23 +5,23 @@ import CartHTML from './CartHTML';
 // import imgCong from '../../img/icon/add.png'
 // import imgTru from '../../img/icon/minus.png'
 function Cart() {
-  const products = JSON.parse(localStorage.getItem('cart'))
-
-  const [state,setState] = useState(products)
-
-    function convertMoney(num) {
-        return num.toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
-      }
   
-  function handleClickBuy(){
+  const products = JSON.parse(localStorage.getItem('cart'))
+  function convertMoney(num) {
+    return num.toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
+}
+function handleClickBuy(){
     alert('Order Success !!! Thanks bro ')
   }
-  // handle total price
-  const totals = products.reduce(function(accumulator,currentValue){
+  const [state,setState] = useState(products)
+  // total Prices
+  // const productCart = document.querySelectorAll('.contentProductCart .total')
+  var totals = products.reduce(function(accumulator,currentValue){
     const total = accumulator + currentValue.price
     return total
-  },0)
-  
+  },0)  
+  // document.querySelectorAll('.')
+  // console.log(totals)
   useEffect(()=>{
     function handleCartNull(){
       const productCart = document.querySelector('.contentProductCart')
@@ -49,11 +49,9 @@ function Cart() {
         })
     }
     handleRemoveProductCart()
-    
-    
-    
+
   },[])
-  // console.log(onChange)
+  // console.log(products)
   return (
     <div className='detailCart'>
       <div className='left'> 
@@ -67,22 +65,22 @@ function Cart() {
       })}
     </div>
       <div className='right'>
-      <h1>SHOPPING CART</h1>
-          <div className='infoUserCart'>
-            <input alt='text' placeholder='FIRST NAME'></input>
-            <input alt='text' placeholder='LAST NAME'></input>
-            <input alt='number' placeholder='YOUR PHONE NUMBER'></input>
-            <input alt='text' placeholder='ADDRESS'></input>
-            <input alt='text' placeholder='NOTE'></input>
-          </div>
-          <div className="total">
-            <div className="totalPrice">
-              {/* <p>SHIPPING : 30k</p> */}
-              <p>TOTAL PRICE : <span>{convertMoney(totals)}</span></p>
+            <h1>SHOPPING CART</h1>
+            <div className='infoUserCart'>
+              <input alt='text' placeholder='FIRST NAME'></input>
+              <input alt='text' placeholder='LAST NAME'></input>
+              <input alt='number' placeholder='YOUR PHONE NUMBER'></input>
+              <input alt='text' placeholder='ADDRESS'></input>
+              <input alt='text' placeholder='NOTE'></input>
             </div>
-            <button onClick={handleClickBuy}>BUY NOW</button>
-          </div>
-      </div>
+            <div className="total">
+              <div className="totalPrices">
+                {/* <p>SHIPPING : 30k</p> */}
+                <p>TOTAL PRICE : <span>{convertMoney(totals)}</span></p>
+              </div>
+              <button onClick={handleClickBuy}>BUY NOW</button>
+            </div>
+        </div>
     </div>
   ) 
   
--- src/components/cart/CartHTML.js
+++ src/components/cart/CartHTML.js
@@ -1,38 +1,42 @@
-import React from 'react'
-import {  useState } from 'react';
+import React,{  useState }  from 'react'
+
 
 import imgClose from '../../img/icon/close.png'
 
 function CartHTML({product}) {
-    const [onChanges,setOnchanges] = useState()
+    // const products = JSON.parse(localStorage.getItem('cart'))
+    const [onChanges,setOnchanges] = useState(1)
+
     function convertMoney(num) {
         return num.toLocaleString('it-IT', { style: 'currency', currency: 'VND' });
     }
-    
+
   return (
+
     <div className="contentProductCart">
-            
-            <div className='detailCartMini'>
-              <div className='imgCart'>
-                {/* <img src={products.images}></img> */}
-                <img src={product.images} alt="react"></img>
-              </div>
-              <div className='detailProductCart'>
-                <h5>{product.name}</h5>
-                <h6>SIZE / {convertMoney(product.price * onChanges)}</h6>
-                <div>
-
-                  <input type='number' onChange={(value)=>setOnchanges(value.target.value)} defaultValue='1'></input>
-
-                </div>
-                {/* <p className='totalPrice'> = {convertMoney(totals)} </p> */}
-              </div>
+        <div className='detailCartMini'>
+            <div className='imgCart'>
+            <img src={product.images} alt="react"></img>
             </div>
-            <div className='imgClose'>
-              <img src={imgClose} ></img>
+            <div className='detailProductCart'>
+            <h5>{product.name}</h5>
+            <h6 className='totalPrice'>{convertMoney(product.price * onChanges)  }</h6> 
+            <div>
+
+                <input type='number' min='1' onChange={(value)=>setOnchanges(value.target.value)} defaultValue='1'></input>
+
             </div>
-          </div>
-  )
+            {/* <p className='totalPrice'> = {convertMoney(totals)} </p> */}
+            </div>
+        </div>
+        <div className='imgClose'>
+            <img src={imgClose} ></img>
+        </div>
+        {/* <div >{}</div> */}
+        
+    </div>
+   
+)
 }
 
 export default CartHTML
