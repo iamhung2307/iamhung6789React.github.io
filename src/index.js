7f03ec5fb15574cfee28fae6b85e252b538ec4e1
haitl <haitl@gisx.vn>
2022-10-08 19:35:51 +0700

    add redux-persist to save cart after reload

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/index.scss';
@@ -16,7 +17,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
