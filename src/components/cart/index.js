14a753efb1a38dd1c0f9eb43dcf55f69191d9f25
Nguyễn Vĩnh Hưng <113844426+iamhung2307@users.noreply.github.com>
2022-10-24 18:34:35 +0700

    pay detail

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -1,6 +1,6 @@
 // import { useState } from "react";
 // import { useEffect, useState } from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import CartItem from './cartItem';
 
 import { convertMoney } from '../../utils';
@@ -11,39 +11,41 @@ import { useState } from 'react';
 import imgMomo from '../../assets/img/icon/logoMomo.svg'
 import imgZaloPay from '../../assets/img/icon/logoZaloPay.svg'
 import imgVNPay from '../../assets/img/icon/logoVNPay.svg'
-import imgQrMomo from '../../assets/img/icon/logoMomo.svg'
+import imgQRMomo from '../../assets/img/icon/imgQRMomo.jpg'
+import imgQRZalopay from '../../assets/img/icon/imgQRZalopay.jpg'
+// import imgQRVNPAY from '../../assets/img/icon/imgQRVNPAY.jpg'
 function Cart() {
-  
+
   const products = useSelector((store) => store.cart);
   // console.log(products)
-  const [firstName,setFirstName] = useState('')
-  const [lastName,setLastName] = useState('')
-  const [phoneNumber,setPhoneNumber] = useState()
-  const [address,setAddress] = useState('')
- 
-  function handleFirstName(e){
-   setFirstName(e.target.value)
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState()
+  const [address, setAddress] = useState('')
+
+  function handleFirstName(e) {
+    setFirstName(e.target.value)
   }
-  function handleLastName(e){
+  function handleLastName(e) {
     setLastName(e.target.value)
   }
-  function handlePhoneNumber(e){
+  function handlePhoneNumber(e) {
     setPhoneNumber(e.target.value)
   }
-  function handleAddress(e){
+  function handleAddress(e) {
     setAddress(e.target.value)
   }
-   
+
   function handleClickBuy() {
-    if(firstName == null || lastName == null || address == null || phoneNumber == null){
+    if (firstName == null || lastName == null || address == null || phoneNumber == null) {
       alert('Complete all information, please')
-    }else if(!(Number(phoneNumber))){
+    } else if (!(Number(phoneNumber))) {
       alert('Check Your Phone Number, please')
-    }else{
+    } else {
       document.querySelector('.order-confirmation').style.display = 'block'
     }
   }
-  function handleBackToCart(){
+  function handleBackToCart() {
     document.querySelector('.order-confirmation').style.display = 'none'
 
   }
@@ -52,53 +54,111 @@ function Cart() {
       accumulator + currentValue.price * currentValue.order_quantity,
     0
   );
-  function handleClickBuyCart(){
-    alert("Thank you for your order. We will contact you soon, Let's keep in touch, pls !!!")
+  
+
+  // handle Pay
+  const inputPay = document.querySelectorAll('.pay input')
+  const imgPay = document.querySelectorAll('.imgPay img')
+  const checkPay = document.querySelector('.checkPay p')
+  const imgQRs = document.querySelectorAll('.checkPay img')
+  // const imgQR = 
+  const isCheckedPay = [...inputPay, ...imgPay]
+  isCheckedPay.forEach((isChecked, index) => {
+    
+    isChecked.onclick = function () {
+      if (index == 0) {
+        checkPay.innerHTML = 'bạn đã chọn thanh toán khi nhận hàng'
+        
+      }else if(index == 1){
+        checkPay.innerHTML = 'Bạn đã chọn thanh toán bằng thẻ'
+        
+      }else if(index==2){
+        document.querySelector('.imgPay').style.display='flex'
+        checkPay.innerHTML = ' '
+      }else if(index==3){
+        if(document.querySelector('.checkPay img.active')){
+          document.querySelector('.checkPay img.active').classList.remove('active');
+        }
+        imgQRs[0].classList.add('active')
+      }else if(index==4){
+        if(document.querySelector('.checkPay img.active')){
+          document.querySelector('.checkPay img.active').classList.remove('active');
+        }
+        imgQRs[1].classList.add('active')
+      }else if(index==5){
+        checkPay.innerHTML = 'Phương thức thanh toán đang được cập nhật. Vui lòng quay lại sau...'
+        if(document.querySelector('.checkPay img.active')){
+          document.querySelector('.checkPay img.active').classList.remove('active');
+        }        
+        // imgQRs[2].classList.add('active')
+      }
+      if(index ==0 || index ==1 ){
+        if(document.querySelector('.checkPay img.active')){
+          document.querySelector('.checkPay img.active').classList.remove('active');
+        }
+        document.querySelector('.imgPay').style.display='none'
+      }else if(index==3||index==4){
+        checkPay.innerHTML = 'Vui lòng quét mã để thanh toán'
+      }
+      
+    }
+  })
+  function handleClickBuyCart() {
+    inputPay.forEach((check)=>{
+      if(document.querySelector('.checkPay img.active')){
+        document.querySelector('.checkPay img.active').classList.remove('active');
+      }
+      if(inputPay[0].checked){
+        checkPay.innerHTML = "Đơn hàng của bạn đã được xác nhận. Vui lòng giữ liên lạc, chúng tôi sẽ gửi đơn hàng đến bạn trong thời gian sớm nhất. Thanks !!!"
+      }else if(check.checked){
+        checkPay.innerHTML ="Đơn hàng của bạn đã được xác nhận. Vui lòng giữ liên lạc, chúng tôi sẽ thông báo cho bạn nếu đơn hàng được thanh toán thành công. Thanks !!!"
+      }
+    })
   }
   return (
     <>
-    <div className='bgTop'></div>
-      <div className="detailCart">
-      <div className="left">
-        {products.length === 0 && (
-          <div className="cartNull">
-            {/* <img src={imgCartNull}></img> */}
-            <p className='textCartNull'>Giỏ hàng của bạn đang trống...</p>
-          </div>
-        )}
+      <div className='bgTop'></div>
+        <div className="detailCart">
+          <div className="left">
+            {products.length === 0 && (
+              <div className="cartNull">
+                {/* <img src={imgCartNull}></img> */}
+                <p className='textCartNull'>Giỏ hàng của bạn đang trống...</p>
+              </div>
+            )}
 
-        {products?.map((product) => {
-          return <CartItem product={product} />;
-        })}
-      </div>
-      {products.length !== 0 && (
-        <div className="right">
-          <h1>SHOPPING CART</h1>
-          <div className="infoUserCart">
-
-            <input defaultValue={firstName} onChange={handleFirstName} placeholder="FIRST NAME" />
-            <input defaultValue={lastName} onChange={handleLastName} placeholder="LAST NAME" />
-            <input defaultValue={phoneNumber} onChange={handlePhoneNumber} placeholder="YOUR PHONE NUMBER" />
-            <input defaultValue={address} onChange={handleAddress} placeholder="ADDRESS" />
-            <input placeholder="NOTE"></input>
+            {products?.map((product) => {
+              return <CartItem product={product} />;
+            })}
           </div>
-          <div className="total">
-            <div className="totalPrices">
-              <p>
-                TOTAL PRICE : <span>{convertMoney(totals)}</span>
-              </p>
+          {products.length !== 0 && (
+            <div className="right">
+              <h1>SHOPPING CART</h1>
+              <div className="infoUserCart">
+
+                <input defaultValue={firstName} onChange={handleFirstName} placeholder="FIRST NAME" />
+                <input defaultValue={lastName} onChange={handleLastName} placeholder="LAST NAME" />
+                <input defaultValue={phoneNumber} onChange={handlePhoneNumber} placeholder="YOUR PHONE NUMBER" />
+                <input defaultValue={address} onChange={handleAddress} placeholder="ADDRESS" />
+                <input placeholder="NOTE"></input>
+              </div>
+              <div className="total">
+                <div className="totalPrices">
+                  <p>
+                    TOTAL PRICE : <span>{convertMoney(totals)}</span>
+                  </p>
+                </div>
+                <button onClick={handleClickBuy}>BUY NOW</button>
+              </div>
             </div>
-            <button onClick={handleClickBuy}>BUY NOW</button>
-          </div>
+          )}
         </div>
-      )}
-    </div>
-    <div className='order-confirmation' >
-      
+      <div className='order-confirmation' >
+
         <div className='content-confirm'>
-        
+
           <div className='left'>
-          
+
             <div>
               <p>Name : {`${firstName} ${lastName}`}</p>
               <p> Phone Number : {phoneNumber}</p>
@@ -112,31 +172,38 @@ function Cart() {
               <p className='Title'>Order confirmation</p>
             </div>
             <div className='bottom'>
-            <div className='pay'>
-              <h6>Thanh Toán</h6>
-              <div>
-                <input type='radio'></input>
-                <p>thanh toán khi nhận hàng</p>
+              <div className='pay'>
+                <h6>Thanh Toán</h6>
+                <div>
+                  <input type='radio' name="drone"></input>
+                  <p>thanh toán khi nhận hàng</p>
+                </div>
+                <div>
+                  <input type='radio' name="drone"></input>
+                  <p>thanh toán bằng thẻ</p>
+                </div>
+                <div>
+                  <input type='radio' name="drone" ></input>
+                  <p>thanh toán bằng ví điện tử</p>
+                </div>
+                <div className='imgPay'>
+                  <img src={imgMomo} ></img>
+                  <img src={imgZaloPay}></img>
+                  <img src={imgVNPay}></img>
+                </div>
               </div>
-              <div>
-                <input type='radio'></input>
-                <p>thanh toán bằng thẻ</p>
+              <div className='checkPay'>
+                <p>Vui lòng chọn hình thức thanh toán</p>
+                <img src={imgQRMomo} className='imgQRMomo' ></img>
+                <img src={imgQRZalopay} className='imgQRZalopay' ></img>
+                {/* <img src={imgQRVNPAY} className='imgQRVNPAY' ></img> */}
+                
               </div>
-              <div className='imgPay'>
-                <img src={imgMomo}></img>
-                <img src={imgZaloPay}></img>
-                <img src={imgVNPay}></img>
-              </div>
-            </div>
-            <div className='checkPay'>
-              {/* <img alt='QR Code Momo' src={imgQrMomo}></img> */}
-              <p>nơi chứa thông tin thanh toán</p>
-            </div>  
               <button onClick={handleClickBuyCart}>Buy Now</button>
             </div>
           </div>
         </div>
-    </div>
+      </div>
     </>
   );
 }
